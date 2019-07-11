8f469ae0691804f288a339674d7f96242a9cf937
jd2rogers2 <jd2rogers2@gmail.com>
2019-07-11 15:15:37 -0400

    server redesign to have websockets inside mongo instead of vice versa

--- server/index.js
+++ server/index.js
@@ -3,42 +3,41 @@
 // {name: "room1"}
 // ids provided by mongodb
 
+const MONGO_URL = 'mongodb+srv://joeyj:' + process.env.MONGO_PWD + '@learning-websockets-agm6f.mongodb.net/test?retryWrites=true&w=majority';
+const mongoOptions = {useNewUrlParser: true, poolSize: 10, reconnectTries: Number.MAX_VALUE, reconnectInterval: 1000};
+const MongoClient = require('mongodb').MongoClient;
+const mongoClient = new MongoClient(MONGO_URL, mongoOptions);
+
 const WEBSOCKET_PORT = 8000;
 const webSocketServer = require('websocket').server;
 const server = require('http').createServer();
-
-const MONGO_URL = 'mongodb+srv://joeyj:' + process.env.MONGO_PWD + '@learning-websockets-agm6f.mongodb.net/test?retryWrites=true&w=majority';
-const MongoClient = require('mongodb').MongoClient;
-// , poolSize: 10, reconnectTries: Number.MAX_VALUE, reconnectInterval: 1000
-// const client = new MongoClient(MONGO_URL, {useNewUrlParser: true, poolSize: 10, reconnectTries: Number.MAX_VALUE, reconnectInterval: 1000});
-const mongoOptions = {useNewUrlParser: true, poolSize: 10, reconnectTries: Number.MAX_VALUE, reconnectInterval: 1000};
-
 server.listen(WEBSOCKET_PORT);
 const wsServer = new webSocketServer({
   httpServer: server
-  // httpServer: client
 });
 
-wsServer.on('request', request => {
-  const connection = request.accept(null, request.origin);
-  connection.on('message', message => {
-    const data = JSON.parse(message.utf8Data);
+mongoClient.connect(err => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log("mongodb connected");
+    wsServer.on('request', request => {
+      const connection = request.accept(null, request.origin);
 
-    if (data.type === 'newMsg') {
-      MongoClient.connect(MONGO_URL, mongoOptions, err => {
-        if (err) {
-          console.log(err);
-        } else {
+      connection.on('message', message => {
+        const data = JSON.parse(message.utf8Data);
+
+        if (data.type === 'newMsg') {
           // , userId: data.userId});
-          MongoClient.db('learning-websockets').collection('messages').insertOne({content: data.content}).then(resp => {
+          mongoClient.db('learning-websockets').collection('messages').insertOne({content: data.content}).then(resp => {
             const newMsg = resp.ops[0];
+            console.log("new message sent to db successfully. id: " + newMsg._id);
             connection.send(JSON.stringify({type: 'newMsg', message: newMsg}));
-          }).catch(blach => {
-            console.log(blach);
+          }).catch(error => {
+            console.log(error);
           });
         }
-
       });
-    }
-  });
+    });
+  }
 });
